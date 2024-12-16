5837efdbd2b1a7d7fc622f3dfbc1d0c65e120a04
Chamaththa123 <shamodchamaththa@gmail.com>
2024-12-17 02:15:22 +0530

    Update inventory.php

--- client/inventory.php
+++ client/inventory.php
@@ -9,8 +9,8 @@ if (!isset($conn)) {
     require_once '../config/config.php';
 }
 
-$user = null; // Initialize to null to avoid undefined variable errors
-$user_id = null; // Initialize user_id
+$user = null;
+$user_id = null;
 
 if (isset($_SESSION['user_email'])) {
     $user_email = $_SESSION['user_email'];
@@ -30,7 +30,7 @@ if (isset($_SESSION['user_email'])) {
 
 if ($user_id !== null) {
   $order_query = $conn->prepare(
-      "SELECT * FROM item" // Make sure the SQL query is correct
+      "SELECT * FROM item"
   );
 
   if ($order_query) {
@@ -46,14 +46,16 @@ if ($user_id !== null) {
 } else {
   die("User ID is null; unable to fetch notifications.");
 }
-
 ?>
+
 <!DOCTYPE html>
 <html>
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../css/sidebar.css">
     <style>
@@ -109,13 +111,9 @@ if ($user_id !== null) {
         padding: 20px;
         padding-left: 16%;
         position: fixed;
-        /* Makes the header fixed */
-        width: 84%;
-        /* Ensures the header spans the full width of the viewport */
+        width: 100%;
         top: 0;
-        /* Fixes the header to the top of the viewport */
         z-index: 900;
-        /* Ensures the header stays above other elements */
     }
 
     .order-container {
@@ -133,11 +131,8 @@ if ($user_id !== null) {
     .items-grid {
         display: grid;
         grid-template-columns: repeat(3, 1fr);
-        /* 3 items per row for larger screens */
         gap: 30px;
-        /* Spacing between items */
         padding: 10px;
-        /* width: 80%; */
     }
 
     .item {
@@ -147,7 +142,6 @@ if ($user_id !== null) {
         padding: 15px;
         text-align: center;
         box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
-        /* width: 20%; */
     }
 
     .item h3 {
@@ -167,10 +161,94 @@ if ($user_id !== null) {
         border: none;
         border-radius: 5px;
         cursor: pointer;
+        font-family: "Outfit", sans-serif;
+        margin-top: 20px
     }
 
     .item .btn:hover {
         background-color: #1a2554;
+    }
+
+    #itemManager {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        display: none;
+        justify-content: center;
+        align-items: flex-start;
+        padding-top: 10px;
+        background-color: rgba(0, 0, 0, 0.4);
+        z-index: 1000;
+    }
+
+    input[type=text],
+    input[type=number] {
+        width: 100%;
+        padding: 6px 10px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        box-sizing: border-box;
+        border-radius: 5px;
+    }
+
+    button {
+        background-color: #2a3577;
+        color: white;
+        padding: 7px 10px;
+        margin: 8px 0;
+        border: none;
+        cursor: pointer;
+        width: 100%;
+        border-radius: 5px;
+    }
+
+    button:hover {
+        opacity: 0.8;
+    }
+
+    .border-radius {
+        border-radius: 10px;
+        max-width: 500px;
+        margin: auto;
+    }
+
+    input[type=text],
+    input[type=password] {
+        width: 100%;
+        padding: 6px 10px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        box-sizing: border-box;
+        border-radius: 5px;
+    }
+
+    .edit-button {
+        background-color: #2a3577;
+        color: white;
+        padding: 7px 10px;
+        margin: 8px 0;
+        border: none;
+        cursor: pointer;
+        width: 50%;
+        border-radius: 5px;
+    }
+
+    button:hover {
+        opacity: 0.8;
+    }
+
+    .input-group {
+        display: flex;
+        gap: 10px;
+        margin-bottom: 15px;
+    }
+
+    .input-item {
+        flex: 1;
     }
 
     @media screen and (max-width: 700px) {
@@ -191,8 +269,7 @@ if ($user_id !== null) {
         }
 
         .items-grid {
-            grid-template-columns: repeat(2, 1fr);
-            /* 2 items per row for medium screens */
+            grid-template-columns: repeat(1, 1fr);
         }
     }
 
@@ -210,7 +287,6 @@ if ($user_id !== null) {
 
         .items-grid {
             grid-template-columns: 1fr;
-            /* 1 item per row for small screens */
         }
     }
     </style>
@@ -236,13 +312,12 @@ if ($user_id !== null) {
                         style="font-size:24px;margin-right:20px;color:#546178"></i></a></div>
         </div>
         <div class="content">
-            <h2>Notification</h2>
-            <div style="background-color: white; border-radius: 10px; padding: 20px;">
+            <div style="background-color: white; border-radius: 10px; padding: 20px;margin-top:80px">
                 <div class="order-container">
                     <div class="items-grid">
                         <?php while ($row = $order_result->fetch_assoc()): ?>
                         <div class="item">
-                            <h3><?php echo htmlspecialchars($row['description']); ?></h3>
+                            <h3 style='font-weight:700'><?php echo htmlspecialchars($row['description']); ?></h3>
                             <div><img src="<?= $row['img_url'] ?>" alt="<?= $row['description'] ?>"
                                     style="width:150px; height:170px;"></div>
                             <div style='text-align:center;font-size:14px; margin-bottom:5px'>
@@ -256,8 +331,10 @@ if ($user_id !== null) {
 
                             <div style='text-align:center;font-size:14px; margin-bottom:5px'>Low Stock Level:
                                 <?php echo htmlspecialchars($row['low_stock_level']); ?></div>
-                            <!-- <p>Price: <?php echo htmlspecialchars($row['item_price']); ?></p> -->
-                            <button class="btn">View Details</button>
+                            <button class='edit-button'
+                                onclick='openEditModal(<?php echo $row["id"]; ?>, "<?php echo addslashes($row["price"]); ?>", "<?php echo addslashes($row["stock"]); ?>")'>Manage
+                                Cylinder</button>
+
                         </div>
                         <?php endwhile; ?>
                     </div>
@@ -267,6 +344,67 @@ if ($user_id !== null) {
 
     </div>
 
+
+    <?php
+    // Handle membership update
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['update_item'])) {
+        $id = $_POST['id'];
+        $price = $_POST['price'];
+        $stock = $_POST['stock'];
+    
+        $stmt = $conn->prepare("UPDATE item SET price = ?, stock = ? WHERE id = ?");
+        $stmt->bind_param("sii", $price, $stock,$id);
+    
+        if ($stmt->execute()) {
+            echo "<script>
+                document.addEventListener('DOMContentLoaded', function () {
+                Swal.fire('Success', 'Manage Cylinder successfully!', 'success').then(() => {
+                        window.location.href = 'inventory.php';
+                    });
+                });
+            </script>";
+            exit;
+        } else {
+            echo "<script>
+                document.addEventListener('DOMContentLoaded', function () {
+                Swal.fire('Error', 'Fail to Manage Cylinder !', 'error').then(() => {
+                        window.location.href = 'inventory.php';
+                    });
+                });
+            </script>";
+            exit;
+        }
+        exit;
+    }
+
+?>
+    <div id="itemManager" class="w3-modal">
+        <div class="w3-modal-content w3-animate-top border-radius">
+            <div class="w3-container">
+                <span onclick="document.getElementById('itemManager').style.display='none'"
+                    class="w3-button w3-display-topright"
+                    style="border-radius: 20px;margin-top:10px;margin-right:10px">&times;</span>
+                <h3>Manage Cylinder</h3>
+                <form method="POST" action="">
+                    <input type="hidden" id="editId" name="id">
+                    <label for="editPrice"><b>Price </b></label>
+                    <input type="number" id="editPrice" name="price" placeholder="Enter Price" required>
+                    <label for="editStock"><b>Stock </b></label>
+                    <input type="number" id="editStock" name="stock" placeholder="Enter Stock" required>
+                    <button type="submit" name="update_item">Manage</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script>
+    function openEditModal(id, price, stock) {
+        document.getElementById('editId').value = id;
+        document.getElementById('editPrice').value = price;
+        document.getElementById('editStock').value = stock;
+        document.getElementById('itemManager').style.display = 'block';
+    }
+    </script>
 </body>
 
 </html>
