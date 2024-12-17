28f065ef290a6d6d3f8b5dae3b3b5038dffcfd8a
Chamaththa123 <shamodchamaththa@gmail.com>
2024-12-18 01:40:34 +0530

    home page web and mobile complete

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
     html {
         height: 100%;
         margin: 0;
+        color: #546178
     }
 
     .hero-image {
@@ -61,6 +62,32 @@
         border-radius: 8px;
     }
 
+    .header1 {
+        font-size: 50px;
+        font-weight: 600;
+        text-align: center;
+        margin-top: 40px;
+    }
+
+    .no {
+        padding: 15px;
+        background-color: #ffd800;
+        border-radius: 50%;
+        color: white;
+        font-weight: bold;
+    }
+
+    .topic {
+        margin-top: 20px;
+        font-weight: 600;
+        font-size: 25px
+    }
+
+    .des {
+        margin-top: 20px;
+        padding: 20px
+    }
+
     @media (min-width: 600px) {
         .container-home {
             grid-template-columns: 1fr 1fr;
@@ -76,48 +103,72 @@
     <!-- Main Content Section -->
     <div class="hero-image">
         <div class="hero-text">
-            <h1 style="font-size:55px;font-weight:650">Your Trusted Gas Partner <br/> Anytime, Anywhere!</h1>
+            <h1 style="font-size:55px;font-weight:650">YOU CLICK. WE'LL DELIVER SUPER QUICK !</h1>
             <p style="font-size:20px;">
-            Experience the convenience of ordering gas cylinders anytime, anywhere.
-            Safe, reliable, and hassle-free delivery right to your doorstep!</p>
+                Experience the convenience of ordering gas cylinders anytime, anywhere.
+                Safe, reliable, and hassle-free delivery right to your doorstep!</p>
             <button onclick="window.location.href='./client/order.php'">Order Now</button>
 
         </div>
     </div>
+    <div class='header1'> Home Delivery </div>
     <div class="container-home">
+
         <div class="column-home">
-            <img src='public/assets/images/StockCake-Indoor Yoga Practice_1731165152.jpg'
-                style='width:80% ;border-radius:15px;height:80%' alt='contact image'>
+            <img src='./images/LITRO.jpg' style='width:90% ;border-radius:15px;height:90%' alt='contact image'>
         </div>
         <div class="column-home">
-            <h2 style='font-weight:600'>Discover Your Perfect Class</h2>
-            <p>FitZone offers a diverse range of classes tailored to meet all fitness levels. From high-energy cardio
-                and strength training to calming yoga and flexibility classes, there’s something for everyone. Dive into
-                our expertly designed classes led by certified trainers who will inspire and support you every step of
-                the way!</p>
-            <button onclick="window.location.href='./client/order.php'" style='width:200px;border-radius:10px'>Explore All
-                Classes</button>
+            <p style='font-size:16px;margin-top:30px'>Our Gas Delivery App is designed to bring unparalleled convenience
+                to your life, ensuring your gas cylinder is just a tap away. With our user-friendly app, you can place
+                orders anytime and have your cylinder delivered directly to your doorstep quickly and reliably.<br />
+
+                We prioritize your safety and satisfaction, offering seamless tracking, secure payments, and prompt
+                customer support to enhance your experience. For any inquiries or assistance, connect with us through
+                our dedicated helpline at 011 - 34 56 768.<br />
+
+                Enjoy the comfort of hassle-free gas delivery and let us make your daily routine simpler and more
+                efficient</p>
+
         </div>
     </div>
+    <div class='header1'> How It Works </div>
     <div class="container-home">
-        <div class="column-home">
-            <h2 style='font-weight:600'>Connect with FitZone</h2>
-            <p>Have questions about our fitness programs or memberships? Our team is here to help! Whether you’re
-                looking for guidance on joining, or need support with ongoing membership, reach out to us anytime. We’re
-                passionate about helping you achieve your fitness goals and are just one click away!</p>
-            <button onclick="window.location.href='contactus.php'" style='width:200px;border-radius:10px'>Get in
-                Touch</button>
+        <div class="column-home"
+            style="float: right; width: 90%; margin-top: 40px; border-radius: 15px; height: 80%; text-align: center;">
+            <div><span class='no'>01</span><img src='./images/online-order.png' style='width:80px;margin-left:20px'
+                    alt='contact image'></div>
+            <div class='topic'>Place Your Order</div>
+            <div class='des'>Visit our website, select your preferred gas cylinder type, and place your order in just a
+                few clicks.</div>
         </div>
         <div class="column-home"
             style="float: right; width: 90%; margin-top: 40px; border-radius: 15px; height: 80%; text-align: center;">
-            <img src="public/assets/images/StockCake-Group Workout Session_1729583555.jpg"
-                style="width: 100%; height: 100%; border-radius: 15px;" alt="contact image">
+            <div><span class='no'>02</span><img src='./images/cashless-payment.png' style='width:80px;margin-left:20px'
+                    alt='contact image'></div>
+            <div class='topic'>Secure Payment</div>
+            <div class='des'>Complete your order with secure online payment options. Pay effortlessly using multiple
+                payment methods, ensuring a smooth and convenient checkout experience.</div>
+        </div>
+        <div class="column-home"
+            style="float: right; width: 90%; margin-top: 40px; border-radius: 15px; height: 80%; text-align: center;">
+            <div><span class='no'>03</span><img src='./images/doorstep-delivery.png' style='width:80px;margin-left:20px'
+                    alt='contact image'></div>
+            <div class='topic'>Doorstep Delivery</div>
+            <div class='des'>Sit back and relax! Our team will deliver your gas cylinder safely and promptly to your
+                doorstep at your chosen time.</div>
+        </div>
+        <div class="column-home"
+            style="float: right; width: 90%; margin-top: 40px; border-radius: 15px; height: 80%; text-align: center;">
+            <div><span class='no'>04</span><img src='./images/tracking.png' style='width:80px;margin-left:20px'
+                    alt='contact image'></div>
+            <div class='topic'>Stay Connected</div>
+            <div class='des'>Track your order status in real-time through our app, and for any inquiries, our customer
+                support is just a call away!</div>
         </div>
 
 
     </div>
-
-    <!-- <?php include 'src/includes/footer.php'; ?> -->
+    <?php include './includes/footer.php'; ?>
 </body>
 
 </html>
