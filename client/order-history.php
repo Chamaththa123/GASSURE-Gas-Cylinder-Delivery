2346be96ac046a1a3f5d71a5a6244815f0f87ccf
Chamaththa123 <shamodchamaththa@gmail.com>
2024-12-19 02:54:27 +0530

    order status display using progress bar

--- client/order-history.php
+++ client/order-history.php
@@ -78,67 +78,94 @@ if ($user_id !== null) {
         <div style='margin-top:50px;text-align:left;font-size:35px ;font-weight:700;margin-bottom:20px;color: #546178'>
             My Orders</div>
 
-        <div class='order-container'>
-            <hr style="height: 1px; background-color: #b0b0b1; border: none;" />
+            <div class='order-container'>
+    <hr style="height: 1px; background-color: #b0b0b1; border: none;" />
 
-            <?php while ($row = $order_result->fetch_assoc()): ?>
-            <div>
+    <?php while ($row = $order_result->fetch_assoc()): ?>
+    <div>
+        <table style='width:100%'>
+            <div style='text-align:center;font-size:14px'>In Progress</div>
+            <div style="display: flex; align-items: center; gap:10px">
+                <?php
+                $status = htmlspecialchars($row['status']);
+                $leftDotColor = '#b0b0b1';
+                $barStyle = "background-color: #b0b0b1;";
+                $rightDotColor = '#b0b0b1';
 
-                <table style='width:100%'>
-                    <tr>
-                        <td>ORDER PLACED<br /><span
-                                style='font-size:13px'><?php echo htmlspecialchars(date('Y-m-d', strtotime($row['order_date']))); ?></span>
-                        </td>
+                switch ($status) {
+                    case 'Pending':
+                        $leftDotColor = 'orange';
+                        break;
+                    case 'In Progress':
+                        $leftDotColor = 'blue';
+                        $barStyle = "background: linear-gradient(to right, blue 50%, #b0b0b1 50%);";
+                        break;
+                    case 'Delivered':
+                        $leftDotColor = 'green';
+                        $barStyle = "background-color: green;";
+                        $rightDotColor = 'green';
+                        break;
+                }
+                ?>
 
-                        <td>TOTAL<br /><span style='font-size:13px'>Rs.
-                                <?php echo htmlspecialchars($row['totalAmount']); ?></span>
-                        </td>
-                        <td style='text-align:right'>ORDER :
-                            OR#<?php echo htmlspecialchars($row['orderId']); ?><br />
-                            <?php
-$status = htmlspecialchars($row['status']);
-$color = '';
-
-switch ($status) {
-    case 'Pending':
-        $color = 'font-weight:bold;color: orange;padding:2px;background-color:#fed7af;border-radius:10px;padding-left:15px;padding-right:15px';
-        break;
-    case 'In Progress':
-        $color = 'font-weight:bold;color: blue;padding:2px;background-color:#71afff;border-radius:10px;padding-left:15px;padding-right:15px';
-        break;
-    case 'Delivered':
-        $color = 'font-weight:bold;color: green;padding:2px;background-color:#6dff49;border-radius:10px;padding-left:15px;padding-right:15px';
-        break;
-    default:
-        $color = 'font-weight:bold;color: black;padding:2px;background-color:orange;border-radius:10px;padding-left:15px;padding-right:15px';
-        break;
-}
-?>
-<span style="font-size:13px; <?php echo $color; ?>"><?php echo $status; ?></span>
-</td>
-                    </tr>
-                    <tr>
-                        <td style='width:20%'> <img src="<?php echo htmlspecialchars($row['img_url']); ?>"
-                                alt="Item Image" style="width: 100px; height: auto;"></td>
-                        <td style='width:40%'><span
-                                style='font-weight:bold;font-size:17px'><?php echo htmlspecialchars($row['description']); ?></span>
-                            <br /><span>Unit Price : Rs.<?php echo htmlspecialchars($row['price']); ?></span>
-                            <br /><span>Qty : <?php echo htmlspecialchars($row['quantity']); ?></span>
-                        </td>
-                        <td style='width:40%;text-align:right'>
-                            <?php echo htmlspecialchars($row['delivery_name']); ?><br />
-                            <?php echo htmlspecialchars($row['delivery_address']); ?><br />
-                            <?php echo htmlspecialchars($row['contact']); ?>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-                <hr style="height: 1px; background-color: #b0b0b1; border: none;" />
+                <div style="width: 13px; height: 13px; background-color: <?php echo $leftDotColor; ?>; border-radius: 50%;"></div>
+                <div style="width: 98%; height: 5px; <?php echo $barStyle; ?>"></div>
+                <div style="width: 13px; height: 13px; background-color: <?php echo $rightDotColor; ?>; border-radius: 50%;"></div>
+            </div>
+            <div style="display: flex; justify-content: space-between; width: 100%;margin-bottom:15px;font-size:14px">
+                <div>Ordered</div>
+                <div>Delivered</div>
             </div>
 
+            <tr>
+                <td>ORDER PLACED<br /><span style='font-size:13px'><?php echo htmlspecialchars(date('Y-m-d', strtotime($row['order_date']))); ?></span>
+                </td>
 
-            <?php endwhile; ?>
-        </div>
+                <td>TOTAL<br /><span style='font-size:13px'>Rs.
+                        <?php echo htmlspecialchars($row['totalAmount']); ?></span>
+                </td>
+                <td style='text-align:right'>ORDER :
+                    OR#<?php echo htmlspecialchars($row['orderId']); ?><br />
+                    <?php
+                    $color = '';
+
+                    switch ($status) {
+                        case 'Pending':
+                            $color = 'font-weight:bold;color: orange;padding:2px;background-color:#fed7af;border-radius:10px;padding-left:15px;padding-right:15px';
+                            break;
+                        case 'In Progress':
+                            $color = 'font-weight:bold;color: blue;padding:2px;background-color:#71afff;border-radius:10px;padding-left:15px;padding-right:15px';
+                            break;
+                        case 'Delivered':
+                            $color = 'font-weight:bold;color: green;padding:2px;background-color:#6dff49;border-radius:10px;padding-left:15px;padding-right:15px';
+                            break;
+                        default:
+                            $color = 'font-weight:bold;color: black;padding:2px;background-color:orange;border-radius:10px;padding-left:15px;padding-right:15px';
+                            break;
+                    }
+                    ?>
+                    <span style="font-size:13px; <?php echo $color; ?>"> <?php echo $status; ?> </span>
+                </td>
+            </tr>
+            <tr>
+                <td style='width:20%'> <img src="<?php echo htmlspecialchars($row['img_url']); ?>"
+                        alt="Item Image" style="width: 100px; height: auto;"></td>
+                <td style='width:40%'><span style='font-weight:bold;font-size:17px'><?php echo htmlspecialchars($row['description']); ?></span>
+                    <br /><span>Unit Price : Rs.<?php echo htmlspecialchars($row['price']); ?></span>
+                    <br /><span>Qty : <?php echo htmlspecialchars($row['quantity']); ?></span>
+                </td>
+                <td style='width:40%;text-align:right'>
+                    <?php echo htmlspecialchars($row['delivery_name']); ?><br />
+                    <?php echo htmlspecialchars($row['delivery_address']); ?><br />
+                    <?php echo htmlspecialchars($row['contact']); ?>
+                </td>
+            </tr>
+        </table>
+        <hr style="height: 1px; background-color: #b0b0b1; border: none;" />
+    </div>
+    <?php endwhile; ?>
+</div>
+
     </div>
     <?php include '../includes/footer.php'; ?>
 </body>
